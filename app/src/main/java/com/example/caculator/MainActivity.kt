1931d3616ec94b198c0452d2c2b4d82bdc3aa6d1
Pumelela-Banca <pumelelabanca@gmail.com>
2025-07-28 14:39:09 +0200

    added lanscape mode

--- app/src/main/java/com/example/caculator/MainActivity.kt
+++ app/src/main/java/com/example/caculator/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.caculator
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var result: EditText
+
+    // hold values for type of operators
+    private var operand
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
+
     }
 }
